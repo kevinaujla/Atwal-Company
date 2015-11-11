6a8df73ac50622d8d9505da0614854fc6a4a36c1
Kevin Aujla <kevinaujla@gmail.com>
2015-11-11 12:59:12 -0800

    added success view upon sent message

--- app/contact.php
+++ app/contact.php
@@ -22,5 +22,5 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: AtwalCoINQUIRY@yourwebsite.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";    
 mail($to,$email_subject,$email_body,$headers);
-header("Location:http://www.atwalco.com/#/view1");         
+header("Location:http://www.atwalco.com/#/view7");         
 ?>
