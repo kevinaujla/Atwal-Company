8eb66de429e8b59804b9bbf6ad819834550d9fc3
Kevin Aujla <kevinaujla@gmail.com>
2015-12-06 16:45:38 -0800

    [feat] : add carousel controller

---
+++ app/carouselController.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('myApp.CarouselDemoCtrl',['ngAnimate', 'ui.bootstrap'])
+
+.controller('CarouselDemoCtrl', function ($scope) {
+  $scope.myInterval = 2000;
+  $scope.noWrapSlides = false;
+  var slides = $scope.slides = [
+    {
+      image: '/Content/sacSkyline.jpg',
+      text: 'Insert Caption'
+    },
+    {
+      image: '/Content/courtroom.jpg',
+      text: 'Insert Caption'
+    },
+    {
+      image: '/Content/immigration pic.jpg',
+      text: 'Insert Caption'
+    },
+    {
+      image: '/Content/orchards7.jpg',
+      text: 'Insert Caption'
+    }
+  ];
+});
