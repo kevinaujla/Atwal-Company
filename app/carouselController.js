48007d023c67d45c49f862c8d2490f3367ca24e8
Kevin Aujla <kevinaujla@gmail.com>
2015-12-08 17:10:40 -0800

    [enhance] : add more styling

--- app/carouselController.js
+++ app/carouselController.js
@@ -8,19 +8,19 @@ angular.module('myApp.CarouselDemoCtrl',['ngAnimate', 'ui.bootstrap'])
   var slides = $scope.slides = [
     {
       image: '/Content/sacSkyline.jpg',
-      text: 'A Full Service Law Firm'
+      text: 'Atwal & Company'
     },
     {
       image: '/Content/courtroom.jpg',
-      text: 'Insert Caption'
+      text: 'A Full Service Law Firm'
     },
     {
       image: '/Content/immigration pic.jpg',
-      text: 'Insert Caption'
+      text: 'Immigration'
     },
     {
       image: '/Content/orchards7.jpg',
-      text: 'Insert Caption'
+      text: 'Agriculture'
     }
   ];
 });
